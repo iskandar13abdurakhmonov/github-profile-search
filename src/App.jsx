9aac88fc66bdd1d01807c3cdc318f63866bbdb99
Iskandar Abdurakhmonov <iskandar.13.aburahmonov@gmail.com>
2023-11-21 08:30:59 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+    return (
+        <div className="wrapper">
+            <header className="header">
+                <div className="header__container">header</div>
+            </header>
+            <main className="main">
+                <div className="main__container">container</div>
+            </main>
+            <footer className="footer">
+                <div className="footer__container">footer</div>
+            </footer>
+        </div>
+    )
+}
+
+export default App
