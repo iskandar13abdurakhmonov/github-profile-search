f5f059933afac8416ac88a5372bb159e774b2b6e
Iskandar Abdurakhmonov <iskandar.13.aburahmonov@gmail.com>
2023-11-22 14:11:19 +0500

    added tailwind css

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -9,8 +9,14 @@ export default function Form() {
     console.log(query)
 
     return (
-        <form onSubmit={(e) => {e.preventDefault(), fetchUser(query), fetchRepos(query)}}>
+        <form 
+            onSubmit={(e) => {
+                e.preventDefault(), fetchUser(query), fetchRepos(query)
+            }}
+        >
+            <label className='text-red-500'>Label</label>
             <input
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w w-48 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 type="text"
                 placeholder="enter username"
                 value={query}
